97a62805c0c324e900f3dd3438bbfa65a9899923
Christian Kaufhold <christian.kaufhold@bsb-muenchen.de>
2023-08-02 10:09:54 +0200

    Wechsel auf standard react bootstrap mit header Leiste

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Bookviewer mit ReactJS, generiert mit Google Bard
 ## Environment
 sudo cp -r node-v18.17.0-linux-x64/{bin,include,lib,share} /usr/
 npm install --save react react-dom react-scripts
-sudo npm install react-bootstrap-validation --save
+npm install bootstrap
 
 ## Tools
 create-react-app bookviewer-reactjs
--- src/App.js
+++ src/App.js
@@ -1,103 +1,51 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import { Container, Row, Col, Nav, NavLink, Card } from "react-bootstrap";
-/*
-import logo from './logo.svg';
-import './App.css';
-*/
 
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
-    <div className="split-window">
-      <div className="left-nav">
-        <ul>
-          <li>Navi 1</li>
-          <li>Navi 2</li>
-          <li>Navi 3</li>
-        </ul>
-      </div>
-      <div className="right-image">
-        <img src="https://placehold.it/1000x500" alt="Image" />
-      </div>
-    </div>
-  );
-}
-*/
-function App() {
-  const [books, setBooks] = useState([
-    {
-      title: "The Lord of the Rings",
-      author: "J.R.R. Tolkien",
-      image: "https://placehold.it/1000x500/000/fff",
-    },
-    {
-      title: "Harry Potter and the Sorcerer's Stone",
-      author: "J.K. Rowling",
-      image: "https://placehold.it/1000x500/00f/fff",
-    },
-    {
-      title: "The Hunger Games",
-      author: "Suzanne Collins",
-      image: "https://placehold.it/1000x500/f00/fff",
-    },
-  ]);
-
-  return (
-    <div>
-      <header className="header">
-        <h1>BookViewer</h1>
-      </header>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/books">Books</NavLink>
-          </li>
-          <li>
-            <NavLink to="/authors">Authors</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <section className="books">
-          <h2>Books</h2>
-          <ul>
-            {books.map((book) => (
-              <li key={book.id}>
-                <Card>
-                  <Card.Img src={book.image} alt={book.title} />
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <Card.Text>{book.author}</Card.Text>
-                  </Card.Body>
-                </Card>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </main>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
