44361cd31b0562b4e11f329bb0592fc410745782
Christian Kaufhold <christian.kaufhold@bsb-muenchen.de>
2023-08-04 08:42:21 +0200

    bsb nummer oeffnen form

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,10 +1,9 @@
-
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function AppHeader() {
   return (
@@ -15,7 +14,7 @@ function AppHeader() {
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link href="#action1">Home</Nav.Link>
@@ -33,7 +32,17 @@ function AppHeader() {
             <Nav.Link href="#" disabled>
               Link
             </Nav.Link>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="BSB Nummer"
+                className="me-2"
+                aria-label="nummer"
+              />
+              <Button variant="outline-success">Open</Button>
+            </Form>
           </Nav>
+
           <Form className="d-flex">
             <Form.Control
               type="search"
