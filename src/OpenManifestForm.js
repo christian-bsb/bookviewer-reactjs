2d8165f9b86dad1025241e8c6800504717e18954
Christian Kaufhold <christian.kaufhold@bsb-muenchen.de>
2023-08-08 10:53:31 +0200

    code der OpenManifestForm aufgeraeumt

--- src/OpenManifestForm.js
+++ src/OpenManifestForm.js
@@ -1,42 +1,41 @@
 import { useState, useContext } from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-import {ManifestContext} from "./App";
+import { ManifestContext } from "./App";
 
 function OpenManifestForm() {
   const [manifest, setManifest] = useContext(ManifestContext);
   const [localid, setLocalId] = useState("bsb");
 
-
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    fetch(`https://api.digitale-sammlungen.de/iiif/presentation/v2/${localid}/manifest`)
-      .then(response => response.json())
-      .then(data => {
+    fetch(
+      `https://api.digitale-sammlungen.de/iiif/presentation/v2/${localid}/manifest`
+    )
+      .then((response) => response.json())
+      .then((data) => {
         setManifest(data);
       });
-    alert(`The id you entered was: ${localid}`);
-  }
+  };
 
   return (
-  <Form className="d-flex" onSubmit={handleSubmit}>
-                <Form.Control
-                  type="text"
-                  placeholder="BSB Nummer"
-                  className="me-2"
-                  value={localid}
-                  onChange={(e) => {
-                  setLocalId(e.target.value);
-                  }
-                  }
-                />
-                <Button variant="outline-success" type="submit">Open {localid}</Button>
-              </Form>
-  )
+    <Form className="d-flex" onSubmit={handleSubmit}>
+      <Form.Control
+        type="text"
+        placeholder="BSB Nummer"
+        className="me-2"
+        value={localid}
+        onChange={(e) => {
+          setLocalId(e.target.value);
+        }}
+      />
+      <Button variant="outline-success" type="submit">
+        Open
+      </Button>
+    </Form>
+  );
 }
 
 export default OpenManifestForm;
-
