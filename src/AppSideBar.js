0af91ace7136e4bd75fcd2e737799f376b31a289
Christian Kaufhold <christian.kaufhold@bsb-muenchen.de>
2023-08-08 15:01:24 +0200

    navbar mit accordion

--- src/AppSideBar.js
+++ src/AppSideBar.js
@@ -1,6 +1,7 @@
 import { useState, useContext } from "react";
 
 import Nav from "react-bootstrap/Nav";
+import Accordion from 'react-bootstrap/Accordion';
 
 import { ManifestContext } from "./App";
 
@@ -9,14 +10,24 @@ function AppSideBar() {
   const m = JSON.stringify(manifest);
 
   return (
-    <Nav defaultActiveKey="/home" className="flex-column">
-      <h3>manifest</h3>
-      <div>
-        {Object.keys(manifest).map((data) => {
-          return <Nav.Link eventKey="link-2">{data}</Nav.Link>;
+      <Accordion>
+        {Object.keys(manifest).map((data, i) => {
+        console.log("daata:" + data + " i: " + i);
+        return (
+          <Accordion.Item key={i} eventKey={i} >
+          <Accordion.Header>{data}</Accordion.Header>
+          <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+          </Accordion.Item>);
         })}
-      </div>
-    </Nav>
+      </Accordion>
   );
 }
 
