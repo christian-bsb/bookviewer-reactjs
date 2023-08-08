09eed9dae369b8fb915d5b9d93134f3a70797e84
Christian Kaufhold <christian.kaufhold@bsb-muenchen.de>
2023-08-08 15:09:15 +0200

    AppSideBar aufgeraeumt

--- src/AppSideBar.js
+++ src/AppSideBar.js
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
 
 import Nav from "react-bootstrap/Nav";
-import Accordion from 'react-bootstrap/Accordion';
+import Accordion from "react-bootstrap/Accordion";
 
 import { ManifestContext } from "./App";
 
@@ -10,24 +10,24 @@ function AppSideBar() {
   const m = JSON.stringify(manifest);
 
   return (
-      <Accordion>
-        {Object.keys(manifest).map((data, i) => {
-        console.log("daata:" + data + " i: " + i);
+    <Accordion>
+      {Object.keys(manifest).map((key, i) => {
         return (
-          <Accordion.Item key={i} eventKey={i} >
-          <Accordion.Header>{data}</Accordion.Header>
-          <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-          </Accordion.Item>);
-        })}
-      </Accordion>
+          <Accordion.Item key={i} eventKey={i}>
+            <Accordion.Header>{key}</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Accordion.Body>
+          </Accordion.Item>
+        );
+      })}
+    </Accordion>
   );
 }
 
